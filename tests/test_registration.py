b89dfe0dbab09678b77843f5fba2b9c6247a5a1e
Andrey Popovich <lessshealth@gmail.com>
2024-11-08 19:26:51 +1000

    driver added

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,7 +9,7 @@ import time
 class RegistrationTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        service = Service(ChromeDriverManager().install())
+        service = Service("./drivers/chromedriver.exe")
         cls.driver = webdriver.Chrome(service=service)
         cls.driver.get('http://127.0.0.1:5000')
         cls.driver.implicitly_wait(10)
